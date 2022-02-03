48e9a2296435c711de6fc986e6a1fda3bf5da664
alexlieberman22 <32115248+alexlieberman22@users.noreply.github.com>
2022-02-02 22:05:08 -0700

    started joining tables

--- PH-Employee.sql
+++ PH-Employee.sql
@@ -6,23 +6,23 @@
 -- );
 
 -- CREATE TABLE employees(
--- 	emp_no INT,
--- 	birth_date DATE,
--- 	first_name VARCHAR,
--- 	last_name VARCHAR,
--- 	gender VARCHAR,
--- 	hire_date DATE,
--- 	PRIMARY KEY (emp_no)
+--     emp_no INT NOT NULL,
+--     birth_date DATE NOT NULL,
+--     first_name VARCHAR(30) NOT NULL,
+--     last_name VARCHAR(30) NOT NULL,
+--     gender VARCHAR(30) NOT NULL,
+--     hire_date DATE NOT NULL,
+--     PRIMARY KEY (emp_no)
 -- );
 
 -- CREATE TABLE dept_manager (
--- dept_no VARCHAR(4) NOT NULL,
---     emp_no INT NOT NULL,
---     from_date DATE NOT NULL,
---     to_date DATE NOT NULL,
--- 	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
--- 	FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
---     PRIMARY KEY (emp_no, dept_no)
+--   dept_no VARCHAR(4) NOT NULL,
+--   emp_no INT NOT NULL,
+--   from_date DATE NOT NULL,
+--   to_date DATE NOT NULL,
+--   FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+--   FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
+--   PRIMARY KEY (emp_no, dept_no)
 -- );
 
 -- CREATE TABLE salaries (
@@ -34,13 +34,70 @@
 --   PRIMARY KEY (emp_no)
 -- );
 
+-- CREATE TABLE dept_emp (
+--   emp_no INT NOT NULL,
+--   dept_no VARCHAR(6) NOT NULL,
+--   from_date DATE NOT NULL,
+--   to_date DATE NOT NULL,
+--   FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
+--   FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+--   PRIMARY KEY (emp_no, dept_no)
+-- );
+
 SELECT * FROM departments;
 SELECT * FROM employees;
+SELECT * FROM dept_manager;
+SELECT * FROM dept_emp;
+SELECT * FROM salaries;
 SELECT * FROM retirement_info;
+SELECT * FROM current_emp;
 
-SELECT first_name, last_name
+DROP TABLE retirement_info;
+DROP TABLE employees CASCADE;
+DROP TABLE dept_emp;
+DROP TABLE current_emp;
+
+
+SELECT emp_no, first_name, last_name
 INTO retirement_info
 From employees
 Where (birth_date Between '1952-01-01' And '1955-12-31')
 	AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
 
+
+SELECT departments.dept_name,
+	dept_manager.emp_no,
+	dept_manager.from_date,
+	dept_manager.to_date
+FROM departments
+INNER JOIN dept_manager 
+ON departments.dept_no = dept_manager.dept_no;
+
+
+SELECT retirement_info.emp_no,
+    retirement_info.first_name,
+	retirement_info.last_name,
+    dept_emp.to_date
+FROM retirement_info
+LEFT JOIN dept_emp
+ON retirement_info.emp_no = dept_emp.emp_no;
+
+
+SELECT d.dept_name,
+     dm.emp_no,
+     dm.from_date,
+     dm.to_date
+FROM departments as d
+INNER JOIN dept_manager as dm
+ON d.dept_no = dm.dept_no;
+	 
+	 
+SELECT ri.emp_no,
+    ri.first_name,
+    ri.last_name,
+	de.to_date
+INTO current_emp
+FROM retirement_info as ri
+LEFT JOIN dept_emp as de
+ON ri.emp_no = de.emp_no
+WHERE de.to_date = ('9999-01-01');
