2fa73e41810722c7a8877b7b3c7c1fd62a0a8569
Alex Lieberman <alexlieberman55@hotmail.com>
2022-02-05 18:35:18 -0700

    Finished Deliverable 2

--- Employee_Database_Challenge.sql
+++ Employee_Database_Challenge.sql
@@ -22,3 +22,15 @@ GROUP BY title
 ORDER BY 1 DESC;
 
 
+SELECT DISTINCT ON(e.emp_no) e.emp_no, e.first_name, e.last_name, e.birth_date,
+	de.from_date, de.to_date, tt.title
+INTO mentorship_eligibilty
+FROM employees as e
+	INNER JOIN dept_emp as de
+		ON e.emp_no = de.emp_no
+	INNER JOIN titles as tt
+		ON e.emp_no = tt.emp_no
+WHERE (de.to_date = '9999-01-01') AND (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+ORDER BY 1;
+
+
